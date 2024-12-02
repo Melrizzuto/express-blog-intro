27467e2d43cfcf4779f7229a3de56783b836a3e5
Melrizzuto <melrizzuto4@gmail.com>
2024-12-02 15:43:09 +0100

    configuration static assets and added roots : /bacheca, and error 404 app.js

--- app.js
+++ app.js
@@ -4,10 +4,25 @@ const PORT = 3000;
 
 const posts = require('./posts');
 
+// Configuro gli asset statici
+app.use(express.static('public'));
+
 // Rotta principale
 app.get('/', (req, res) => {
     res.send('Server del mio blog');
 });
+
+// Rotta per la bacheca che restituisce i post come JSON
+app.get('/bacheca', (req, res) => {
+    res.json({
+        posts: posts,
+        count: posts.length
+    });
+});
+
+app.all('*', (req, res) => {
+    res.status(404).send(`<h1>Error 404. Page not found</h1>`)
+})
 
 // Avvio del server
 app.listen(PORT, () => {
