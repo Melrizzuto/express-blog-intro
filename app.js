6a0b267492be1c47957fff729dbb572afa99b256
Melrizzuto <melrizzuto4@gmail.com>
2024-12-02 15:30:47 +0100

    added imgs in public folder and created a module posts exported in app.js

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
+const posts = require('./posts');
+
 // Rotta principale
 app.get('/', (req, res) => {
     res.send('Server del mio blog');
